82e41cd1b3b24e2cd950d8016d679f9ac0b8c826
leandroyyy <leandro.cavallari123@gmail.com>
2025-08-18 19:55:42 -0300

    removed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { Header } from "./components/header";
-import { WorkoutCard } from "./components/workout-card";
-
 function App() {
   return (
     <>
-      <Header />
-
-      <WorkoutCard
-        id="1"
-        date="15/08/2025"
-        durationMinutes={60}
-        intensity={5}
-        title="Zumba"
-      />
-
-      <WorkoutCard
-        id="2"
-        date="16/08/2025"
-        durationMinutes={45}
-        intensity={3}
-        title="FitDance"
-      />
+      <h1>Trackfit</h1>
     </>
   );
 }
--- src/components/header.tsx
+++
@@ -1,8 +0,0 @@
-export function Header() {
-  return (
-    <>
-      <h1>Trackfit</h1>
-      <h2>Seja bem vindo ao melhor app de treino</h2>
-    </>
-  );
-}
