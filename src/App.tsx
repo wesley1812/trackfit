68e4bf4228398e7daabdb735046345be35bd1c41
leandroyyy <leandro.cavallari123@gmail.com>
2025-08-18 20:45:45 -0300

    feat: created workout card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Header } from "./components/header";
+import { WorkoutCard } from "./components/workout-card";
 
 function App() {
   return (
     <>
       <Header />
+
+      <main>
+        <WorkoutCard
+          id="1"
+          date="20/05/2026"
+          durationMinutes={45}
+          intensity={4}
+          title="Aula de zumba"
+        />
+      </main>
     </>
   );
 }
